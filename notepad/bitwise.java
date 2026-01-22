db7a875abf8fa5752a1eac4f5b4e50481d9d4c55
Chirag <chiragrrai44@gmail.com>
2026-01-22 14:12:34 +0530

    bitwise operators

--- notepad/bitwise.java
+++ notepad/bitwise.java
@@ -1,9 +1,9 @@
 class bitwise {
 
 	public static void main(String[] args) {
-	System.out,println(5|6);
-	System.out,println(5&6);
-	System.out,println(5^6);
+	System.out.println(5|6);
+	System.out.println(5&6);
+	System.out.println(5^6);
 
 
         
