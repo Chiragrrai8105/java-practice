85573a1e4dd582c5ffcbef8b4f9842e628db1a95
Chirag <chiragrrai44@gmail.com>
2026-01-22 15:51:29 +0530

    increment operators

--- notepad/increment.java
+++ notepad/increment.java
@@ -1,15 +1,17 @@
 class increment {
 
 	public static void main(String[] args) {
+	int x=3,y=5,z=7;
 	int a=10;
 	//System.out.println(a++);
 	//System.out.println(++a);
 	//System.out.println(a++ + ++a);
 	//System.out.println(a++ * ++a + 17 + ++a + a++ - 14 * 3);
 	//System.out.println(a++ * a++ + 17 + ++a + ++a - 14 * 3);
-	System.out.println(a-- * --a + 17 + --a + a-- - 14 * 3);
-
-
+	//System.out.println(a-- * --a + 17 + --a + a-- - 14 * 3);
+	int r=x++ + y-- + ++z + (x>y ? ++x : y--) + (z-- - x++); 
+	//System.out.println(x+""+y+""+z+""+r);
+	System.out.println(r);
 
 
 }
