8beb0b08e2addf9c9a3003c2e5ef46bbea50271c
Chirag <chiragrrai44@gmail.com>
2026-01-22 12:27:45 +0530

    logical

--- notepad/logical.java
+++ notepad/logical.java
@@ -3,5 +3,7 @@ class logical {
 	public static void main(String[] args) {
         // System.out.println(!10==10);
          System.out.println(true || 10==10);
+	 System.out.println(true && 10==10);
+
 }
 }
