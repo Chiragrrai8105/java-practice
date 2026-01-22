8f3cb400818a16649dd467d3125a3df5585da117
Chirag <chiragrrai44@gmail.com>
2026-01-22 11:50:58 +0530

    relational

--- notepad/relational.java
+++ notepad/relational.java
@@ -3,8 +3,12 @@ class relational {
 	public static void main(String[] args) {
 	int a =10 ;
 	int b=10;
+        char c='c';
+	char d='c';
         System.out.println(a==b);
-	 System.out.println(a!=b);
+	System.out.println(a!=b);
+	System.out.println(c==d);
+
 
 }
 }
