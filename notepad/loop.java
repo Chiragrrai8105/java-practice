a43f8c9733ef23da24febab0dd076fc490ce31fd
Chirag <chiragrrai44@gmail.com>
2026-01-23 11:52:49 +0530

    Loop

--- notepad/loop.java
+++ notepad/loop.java
@@ -3,10 +3,18 @@ class loop {
 
 	public static void main(String[] args)
 	{
-	for( int i=0;i<10;i++)
+	/*for( int i=0;i<10;i++)
 	{
 		System.out.println("Hi");
+	}*/
+	int i=1;
+	while(i==5)
+	{
+	System.out.println("hi chirag");
+	i++;
 	}
+	
+
 }
 }
 		
