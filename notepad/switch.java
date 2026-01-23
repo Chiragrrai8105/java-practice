e2caebd69e278637ba0a08bbcf40582d00638d5b
Chirag <chiragrrai44@gmail.com>
2026-01-23 10:10:44 +0530

    switch statements

---
+++ notepad/switch.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+class switch {
+
+	public static void main(String[] args)
+	{
+		System.out.println("1 withdraw");
+		System.out.println("2 Deposit");
+		System.out.println("3 checkbalance");
+		System.out.println("4 Exit");
+		int choice = sc.nextInt();
+		switch(choice){
+		case 1: 
+		{
+			System.out.println("withdraw the amount");
+			break;
+		}
+		case 2: 
+		{
+			System.out.println("deposit the amount");
+			break;
+		}
+		case 3: 
+		{
+			System.out.println("check balance");
+			break;
+		}
+		case 4: 
+		{
+			System.out.println("Exit");
+			break;
+		}
+		System.out.println("Thank you");
+	}
+}
+}
+
+
+
+
+		
+
+		
+		
+
+
+
+
