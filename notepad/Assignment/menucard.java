fdc528f521a1261486184ec5e2b97638086ff71a
Chirag <chiragrrai44@gmail.com>
2026-01-23 19:22:48 +0530

    menu card

--- notepad/Assignment/menucard.java
+++ notepad/Assignment/menucard.java
@@ -41,37 +41,85 @@ class menucard{
 							}
 						case 2:
 							{
-								System.out.println("In South we have Andra pepper chicken -> 1");
-								System.out.println("In South we have chicken sukka -> 2");
-								System.out.println("In South we have chicken chettinad  -> 3");
+								System.out.println("In South we have Andra Pepper Chicken -> 1");
+								System.out.println("In South we have Chicken Sukka -> 2");
+								System.out.println("In South we have Chicken Chettinad  -> 3");
 								int c=sc.nextInt();
 								switch(c)
 								{
 									case 1:
 										{
-											System.out.println("You have choosen Tandoori kofta chicken and the order will be ready in 5 minutes");
+											System.out.println("You have choosen Andra Pepper Chicken and the order will be ready in 5 minutes");
 										}
 									case 2:
 										{
-											System.out.println("You have choosen kadai chicken and the order will be ready in 5 minutes");
+											System.out.println("You have choosen Chicken Sukka and the order will be ready in 5 minutes");
 										}
 									case 3:
 										{
-											System.out.println("You have choosen Hariyali chicken and the order will be ready in 5 minutes");
+											System.out.println("You have choosen Chicken Chettinad and the order will be ready in 5 minutes");
+										}		
+								}
+							}	
+					}
+				}	
+
+			case 2:
+				{
+					System.out.println("Select north as 1");
+					System.out.println("Select south as 2");
+					int b=sc.nextInt();
+					switch(b)
+					{
+						case 1:
+							{
+								System.out.println("In North we have paneer butter Masala -> 1");
+								System.out.println("In North we have  Dal Makhani -> 2");
+								System.out.println("In North we have Baingan Bharta  -> 3");
+								int c=sc.nextInt();
+								switch(c)
+								{
+									case 1:
+										{
+											System.out.println("You have choosen Paneer butter Masala and the order will be ready in 5 minutes");
+										}
+									case 2:
+										{
+											System.out.println("You have choosen Dal Makhani and the order will be ready in 5 minutes");
+										}
+									case 3:
+										{
+											System.out.println("You have choosen Baingan Barta and the order will be ready in 5 minutes");
+										}		
+								}
+							}
+						case 2:
+							{
+								System.out.println("In South we have Rice Sambar -> 1");
+								System.out.println("In South we have Sweet Pongal -> 2");
+								System.out.println("In South we have Masala Dosa -> 3");
+								int c=sc.nextInt();
+								switch(c)
+								{
+									case 1:
+										{
+											System.out.println("You have choosen Andra pepper chicken and the order will be ready in 5 minutes");
+										}
+									case 2:
+										{
+											System.out.println("You have choosen chicken sukka and the order will be ready in 5 minutes");
+										}
+									case 3:
+										{
+											System.out.println("You have choosen chicken chettinad and the order will be ready in 5 minutes");
 										}		
 								}
 							}	
 					}
 
-				}
+				}	
 		}
-
 		
-
-
-		
-		
-			
 		
 }
 }
