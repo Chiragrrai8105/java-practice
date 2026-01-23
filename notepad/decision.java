e1d9d943079e254c8db94a340ce7d7e71fc73acc
Chirag <chiragrrai44@gmail.com>
2026-01-23 09:18:06 +0530

    decision statement

--- notepad/decision.java
+++ notepad/decision.java
@@ -3,9 +3,13 @@ class decision {
 	public static void main(String[] args) {
 	int x=3,y=5,z=7;
 	int a=10;
-    if (x==3)
+    if (x%2==0)
     {
-        System.out.println("x is 3");
+        System.out.println("x is even");
+    }
+    else
+    {
+        System.out.println("x is odd");
     }
     }
 }
