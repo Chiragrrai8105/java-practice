845486656dc0893b5e9f924a1f62d99461525932
Chirag <chiragrrai44@gmail.com>
2026-01-23 19:26:36 +0530

    menu card

--- notepad/Assignment/menucard.java
+++ notepad/Assignment/menucard.java
@@ -28,14 +28,17 @@ class menucard{
 									case 1:
 										{
 											System.out.println("You have choosen Tandoori kofta chicken and the order will be ready in 5 minutes");
+											break;
 										}
 									case 2:
 										{
 											System.out.println("You have choosen kadai chicken and the order will be ready in 5 minutes");
+											break;
 										}
 									case 3:
 										{
 											System.out.println("You have choosen Hariyali chicken and the order will be ready in 5 minutes");
+											break;
 										}		
 								}
 							}
@@ -50,14 +53,17 @@ class menucard{
 									case 1:
 										{
 											System.out.println("You have choosen Andra Pepper Chicken and the order will be ready in 5 minutes");
+											break;
 										}
 									case 2:
 										{
 											System.out.println("You have choosen Chicken Sukka and the order will be ready in 5 minutes");
+											break;
 										}
 									case 3:
 										{
 											System.out.println("You have choosen Chicken Chettinad and the order will be ready in 5 minutes");
+											break;
 										}		
 								}
 							}	
@@ -82,14 +88,17 @@ class menucard{
 									case 1:
 										{
 											System.out.println("You have choosen Paneer butter Masala and the order will be ready in 5 minutes");
+											break;
 										}
 									case 2:
 										{
 											System.out.println("You have choosen Dal Makhani and the order will be ready in 5 minutes");
+											break;
 										}
 									case 3:
 										{
 											System.out.println("You have choosen Baingan Barta and the order will be ready in 5 minutes");
+											break;
 										}		
 								}
 							}
@@ -103,15 +112,18 @@ class menucard{
 								{
 									case 1:
 										{
-											System.out.println("You have choosen Andra pepper chicken and the order will be ready in 5 minutes");
+											System.out.println("You have choosen Rice Sambar and the order will be ready in 5 minutes");
+											break;
 										}
 									case 2:
 										{
-											System.out.println("You have choosen chicken sukka and the order will be ready in 5 minutes");
+											System.out.println("You have choosen Sweet Pongal and the order will be ready in 5 minutes");
+											break;
 										}
 									case 3:
 										{
-											System.out.println("You have choosen chicken chettinad and the order will be ready in 5 minutes");
+											System.out.println("You have choosen  Masala Dosa and the order will be ready in 5 minutes");
+											break;
 										}		
 								}
 							}	
