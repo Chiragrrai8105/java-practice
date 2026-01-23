1b5a33d99118418484d5936405de8132168454b4
Chirag <chiragrrai44@gmail.com>
2026-01-23 09:49:39 +0530

    decision statement about IF ELSE Ladder

---
+++ notepad/ladder.java
@@ -0,0 +1,29 @@
+class ladder {
+
+	public static void main(String[] args) {
+		int mark = 85;
+		if (mark>=90)
+		{
+			System.out.println("A Grade");
+		}
+		else if (mark>=75)
+		{
+			System.out.println("B Grade");
+		}
+		else if (mark>=60)
+		{
+			System.out.println("C Grade");
+		}
+		 else if (mark>=45)
+		{
+			System.out.println("D Grade");
+		}
+		else 
+		{
+			System.out.println("FAIL");
+		}
+		System.out.println("Thank you");
+	}
+}	
+
+		
