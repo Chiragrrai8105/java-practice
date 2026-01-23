030d6875e63b7c180d13b28df83b9e35e2f77a76
Chirag <chiragrrai44@gmail.com>
2026-01-23 19:03:42 +0530

    menu card

---
+++ notepad/Assignment/menu.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+class  {
+
+	public static void main(String[] args)
+	{	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1 Non veg");
+		System.out.println("2 veg");
+		System.out.println("Enter the choice");
+		int a=sc.nextInt();
+		switch(a)
+		{
+			case 1:
+				{
+					System.out.println("Select:-  north as 1 or south as 2");
+					int b=sc.nextInt();
+					switch(b)
+					{
+						case 1:
+							{
+								System.out.println("In North we have Tandoori kofta chicken -> 1");
+								System.out.println("In North we have  kadai chicken -> 2");
+								System.out.println("In North we have hariyali chicken  -> 3");
+								int c=sc.nextInt();
+								switch(c)
+								{
+									
+								}
+							}
+					}
+
+				}
+		}
+
+		
+
+
+		
+		
+			
+		
+}
+}
+
+
+
+
+		
+
+		
+		
+
+
+
+
