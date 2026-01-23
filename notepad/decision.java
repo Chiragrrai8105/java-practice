b22028b76385a79d4b99ae8ffa3a2fe397fb6414
Chirag <chiragrrai44@gmail.com>
2026-01-23 09:37:15 +0530

    decision statement

--- notepad/decision.java
+++ notepad/decision.java
@@ -2,14 +2,23 @@ class decision {
 
 	public static void main(String[] args) {
 	int x=3,y=5,z=7;
+    int budget = 1000;
 	int a=10;
-    if (x%2==0)
+    /*if (x%2==0)
     {
         System.out.println("x is even");
     }
     else
     {
         System.out.println("x is odd");
+    }*/
+    if (budget<500)
+    {
+        System.out.println(can buy items);
+    }
+    else if ( budget>1000)
+    {
+        System.out.println("cannot buy itmes");
     }
     }
 }
