756d4f99fc7760944b21ca777db3e7183aa86b92
Chirag <chiragrrai44@gmail.com>
2026-01-23 18:48:12 +0530

    leap year

--- notepad/Assignment/leap.java
+++ notepad/Assignment/leap.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 class leap {
 	public static void main(String[] args){
-	Scanner sc = new Scanner();
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the year");
 	int year = sc.nextInt();
 	if(year%4==0 && year%100!=0 || year%400==0)
 	{
