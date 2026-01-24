a10499288f5bcf6586a686bbb484b3b8d03a310e
Chirag <chiragrrai44@gmail.com>
2026-01-24 14:56:21 +0530

    byte data return type

--- notepad/returntype.java
+++ notepad/returntype.java
@@ -4,11 +4,20 @@ class returntype {
 	System.out.println("By chirag");
 	return 500;
 	}
+	public static byte love(){
+	System.out.println("I Love You");
+	System.out.println("By chirag");
+	return 50;
+	}
+
 public static void main(String[] args)
 {
 	System.out.println("Hii Lover");
 	int rs=loveletter();
-	System.out,println("Amount from Lover"+rs);
+	byte s=love();
+	System.out.println("Amount from Lover"+rs);
+	System.out.println("Amount from Lover"+s);
+
 }
 }
 
