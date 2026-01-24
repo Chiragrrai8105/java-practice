cc49931dacf41da344be62c61c6826b492ff3a51
Chirag <chiragrrai44@gmail.com>
2026-01-24 15:30:27 +0530

    parameter passing

--- notepad/parameter.java
+++ notepad/parameter.java
@@ -1,12 +1,17 @@
 class parameter {
-	public static void loveletter(){
+	public static void loveletter(String msg,int amount){
 	System.out.println("I Love You");
 	System.out.println("By chirag");
+	System.out.println(amount);
+	System.out.println(msg);
+
+
+
 	}
 public static void main(String[] args)
 {
 	System.out.println("Hii Lover");
-	loveletter();
+	loveletter("hi How are you",500);
 }
 }
 
