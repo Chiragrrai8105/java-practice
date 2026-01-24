d3a979ba38088a1cc86e46edd40839bd01568b78
Chirag <chiragrrai44@gmail.com>
2026-01-24 13:01:51 +0530

    printing alphabet

---
+++ notepad/alphabet.java
@@ -0,0 +1,16 @@
+class alphabet {
+	public static void main(String[] args){
+	for (int i=65;i<91;i++)
+	{	
+
+		char c=(char)i;
+		System.out.println(c);
+	}
+	for (int j=97;j<123;j++)
+	{	
+		char d=(char)j;
+		System.out.println(d);
+	}
+}
+}
+	
