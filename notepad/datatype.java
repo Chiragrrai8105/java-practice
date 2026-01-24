18853e4a505232c17915dcbddbdb62bd2f6e6393
Chirag <chiragrrai44@gmail.com>
2026-01-24 12:23:29 +0530

    Datatypes TYPES

--- notepad/datatype.java
+++ notepad/datatype.java
@@ -3,9 +3,9 @@ class datatype {
 	public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Enter the Boolean data");
-	/*byte byteData = sc.nextByte();
-	//System.out.println(byteData);
-	//System.out.println("Enter the Int data");
+	byte byteData = sc.nextByte();
+	System.out.println(byteData);
+	System.out.println("Enter the Int data");
 	int intData = sc.nextInt();
 	System.out.println(intData);
 
