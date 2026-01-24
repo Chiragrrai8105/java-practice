7ef6827469616e98a1136c9d08c3c5c5d61e3586
Chirag <chiragrrai44@gmail.com>
2026-01-24 14:49:04 +0530

    love letter

---
+++ notepad/returntype.java
@@ -0,0 +1,15 @@
+class returntype {
+	public static int loveletter(){
+	System.out.println("I Love You");
+	System.out.println("By chirag");
+	return 500;
+	}
+public static void main(String[] args)
+{
+	System.out.println("Hii Lover");
+	int rs=loveletter();
+	System.out,println("Amount from Lover"+rs);
+}
+}
+
+	
