c2cd779464f2d671505323b2391d6b150e0a3672
Chirag <chiragrrai44@gmail.com>
2026-01-24 12:40:50 +0530

    Narrowing Datatype

---
+++ notepad/narrow.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class narrow {
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int a=89;
+	char c=(char)a;
+	System.out.println(c);
+}
+}
