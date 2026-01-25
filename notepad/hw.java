96801e8ed81a5e13a1f45af1e97f88c999eefc41
Chirag <chiragrrai44@gmail.com>
2026-01-25 19:23:42 +0530

    call funtion for prime number

--- notepad/hw.java
+++ notepad/hw.java
@@ -1,8 +1,26 @@
 class hw{
 	public static void prime(int n)
 	{
+		boolean bool=false;
 		for(int i = 2;i<=n-1;i++)
 		{
 			if(n%i==0)
 			{
-							
+				bool=true;
+				break;
+			}
+			else
+			{
+				bool=false;
+			}
+			if(bool==true)
+			{
+				System.out.println("Prime number");
+			}
+			else
+			{
+				System.out.println("Not prime number");
+			}
+		}
+	}
+}							
