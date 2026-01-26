b358d83a933e1df53cf1dea8a68876581071f7d5
Chirag <chiragrrai44@gmail.com>
2026-01-26 12:12:11 +0530

    calling method in another class

---
+++ notepad/nonstatic/demo2.java
@@ -0,0 +1,13 @@
+
+class demo2
+{
+    public static void chirag()
+    {
+        System.out.println("Hi chirag from demo class");
+    }
+    public static void main(String[] args)
+    {
+        
+    }
+
+}
