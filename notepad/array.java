496aec8a0e08b27f4b10c5c66cd58602ceefb628
Chirag <chiragrrai44@gmail.com>
2026-01-27 12:32:13 +0530

    Array creation

--- notepad/array.java
+++ notepad/array.java
@@ -2,11 +2,17 @@ class array
 {
     public static void main(String[] args)
     {
+        String name[] = new String[5];
         int arr[] = new int[10];
-        for (int i=0;i<10;i++)
+        /*for (int i=0;i<10;i++)
         {
             arr[i]=i;
             System.out.println(arr[i]);
+        }*/
+        for (int i=0;i<5;i++)
+        {
+            name[i]="chirag";
+            System.out.println(name[i]);
         }
     }
 }
