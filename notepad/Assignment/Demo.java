d1319762ef49272fca657940e88c6d18d0870794
Chirag <chiragrrai44@gmail.com>
2026-01-27 10:33:25 +0530

    method binding

--- notepad/Assignment/Demo.java
+++ notepad/Assignment/Demo.java
@@ -7,7 +7,7 @@ class Demo
     public static void main(String[] args)
     {
         Demo d = new Demo();
-        //System.out.println(d);
+        System.out.println(d);
         Demo d1 = objref(d);
         System.err.println(d1);
     }
