430a31b7e9842dbe8a5bc1ffd83d0f6dfb66e867
Chirag <chiragrrai44@gmail.com>
2026-01-27 09:32:30 +0530

    method overloading

--- notepad/Assignment/methodoverloading.java
+++ notepad/Assignment/methodoverloading.java
@@ -1,4 +1,16 @@
 class methodoverloading
 {
-    public static void sum
+    public static void sum(int m)
+    {
+        System.out.println("chirag with 1 parameter");
+    }
+     public static void sum(int m,int n)
+    {
+        System.out.println("chirag with 2 parameter");
+    }
+    public static void main(String[] args)
+    {
+        sum(1);
+        sum(1,2);
+    }
 }
