e513ab46cef2f297449bdaca7d013a9bea41e265
Chirag <chiragrrai44@gmail.com>
2026-01-27 14:49:21 +0530

    Array using for each loop and conerting int to char

--- notepad/arrlength.java
+++ notepad/arrlength.java
@@ -2,10 +2,14 @@ class arrlength
 {
     public static void main(String[] args)
     {
-        int a[]={1,2,3,4,5};
+        /*int a[]={1,2,3,4,5};
         System.out.println(a.length);
         char c[]={'a','v'};
-        System.out.println(c.length);
-
+        System.out.println(c.length);*/ 
+        char ch[]={'a',56,'b',98,99};
+        for(char b : ch)
+        {
+            System.out.println(b);
+        }
     }
 }
