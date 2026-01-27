b20b19daac313f51da8c9bd7581cc67f141f0ddb
Chirag <chiragrrai44@gmail.com>
2026-01-27 12:44:55 +0530

    Array creation with string

--- notepad/array.java
+++ notepad/array.java
@@ -1,17 +1,28 @@
+import java.util.Scanner;
 class array
 {
     public static void main(String[] args)
     {
-        String name[] = new String[5];
-        int arr[] = new int[10];
+        Scanner sc = new Scanner(System.in);
+        String name[] = new String[6];
+        //int arr[] = new int[10];
         /*for (int i=0;i<10;i++)
         {
             arr[i]=i;
             System.out.println(arr[i]);
         }*/
-        for (int i=0;i<5;i++)
+        /*for (int i=0;i<5;i++)
         {
             name[i]="chirag";
+            System.out.println(name[i]);
+        }*/
+       for (int i=1;i<=5;i++)
+        {
+            System.out.println("Enter the name of" +i+ "person");
+            name[i]=sc.nextLine();
+        }
+        for(int i=0;i<5;i++)
+        {
             System.out.println(name[i]);
         }
     }
