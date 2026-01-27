236887b7d853e86fbdd2781bbf6c99688f58d1b1
Chirag <chiragrrai44@gmail.com>
2026-01-27 10:17:11 +0530

    object reference

---
+++ notepad/Assignment/objectref.java
@@ -0,0 +1,12 @@
+class objectref
+{
+    public static void demo(objectref ref)
+    [
+        System.out.println(ref);
+    ]
+    public static void main(String[] args)
+    {
+        System.out.println(d);
+        demo(d);
+    }
+}
