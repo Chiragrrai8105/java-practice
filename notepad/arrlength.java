d9c82e3c85b060175a1875d709d713d8d8ee22e5
Chirag <chiragrrai44@gmail.com>
2026-01-27 14:45:01 +0530

    Array creation with string

---
+++ notepad/arrlength.java
@@ -0,0 +1,11 @@
+class arrlength
+{
+    public static void main(String[] args)
+    {
+        int a[]={1,2,3,4,5};
+        System.out.println(a.length);
+        char c[]={'a','v'};
+        System.out.println(c.length);
+
+    }
+}
