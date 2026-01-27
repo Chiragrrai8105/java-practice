42590fff9aaba640f3d3f5a5a4e2bd360969cdc2
Chirag <chiragrrai44@gmail.com>
2026-01-27 11:53:12 +0530

    Armstrong

--- notepad/armstrong.java
+++ notepad/armstrong.java
@@ -4,22 +4,25 @@ class armstrong
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        int num2=0,count=0,num3=0;
+        int count=0,num3=0;
         System.out.println("Enter the number");
         int num=sc.nextInt();
+        int result=num;
         while(num>0)
         {
             int num1=num%10;
             count++;
             num/=10;
         }
-        while(num>0)
+        while(result>0)
         {
-            num2=num%10;
+            int num2=result%10;
             num2=num2^count;
             num3=num3+num2;
+            result/=10;
         }
         System.out.println(num3);
+        System.out.println(count);
         if(num3==num)
         {
             System.out.println("Armstrong");
