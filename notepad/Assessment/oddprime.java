db7e24510555c6ac29df6eb70550aa2b8699e020
Chirag <chiragrrai44@gmail.com>
2026-01-28 18:40:45 +0530

    printing odd prime number in given range

---
+++ notepad/Assessment/oddprime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class oddprime
+{
+    public void demo()
+    {   
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first number");
+        int start=sc.nextInt();
+        System.out.println("enter the last number");
+        int end =sc.nextInt();
+        for(int i=start;i<=end;i++)
+        {
+            for(int j=2;j<i;j++)
+            {
+                if(i%j==0||i%2!=0)
+                {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+    public static oddprime obj()
+    {
+        return new oddprime();
+    }
+    public static void main(String[] args)
+    {
+        obj().demo();
+    }
+}
