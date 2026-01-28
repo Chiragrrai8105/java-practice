b70e964fff76ed05338dc64c40fc31f2cb9f31bf
Chirag <chiragrrai44@gmail.com>
2026-01-28 12:00:23 +0530

    Non static variable and object

---
+++ notepad/student.java
@@ -0,0 +1,14 @@
+class student
+{
+    String name;
+    int id;
+    long phno;
+    public static void main(String[] args)
+    {
+        student chirag;
+        chirag=new student();
+        System.out.println(chirag.name);
+        System.out.println(chirag.id);
+        System.out.println(chirag.phno);
+    }
+}
