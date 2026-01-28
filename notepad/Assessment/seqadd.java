29653904b0c56a6e05bc2a5049a286982b1235cc
Chirag <chiragrrai44@gmail.com>
2026-01-28 19:16:25 +0530

    printing the array numbers until all the array value becomes one digit

--- notepad/Assessment/seqadd.java
+++ notepad/Assessment/seqadd.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 class seqadd
 {
-    public void demo(int ar[])
-    {   int br[]=new int[n];
-        for(int i=0;i<=n;i++)
+    Scanner sc = new Scanner(System.in);
+    public void demo(int ar[],int n)
+    {
+        int br[]=new int[n];
+        for(int i=0;i<n;i++)
         {   
             int d=0;
             while(ar[i]<=9)
@@ -11,10 +14,29 @@ class seqadd
                 {
                     int c=ar[i]%10;
                     d=d+c;
+                    ar[i]/=10;
                 }
-                ar[i]=d;
-                
+                ar[i]=d; 
             }
+            br[i]=sc.nextInt();
+            System.out.println(br[i]);
+        }  
+    }
+    public static seqadd obj()
+    {
+        return new seqadd();
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no of elements");
+        int n=sc.nextInt();
+        int ar[] = new int[n];
+        System.out.println("Enter the elements");
+        for(int i=0;i<n;i++)
+        {
+            ar[i]=sc.nextInt();
         }
+        obj().demo(ar,n);
     }
 }
