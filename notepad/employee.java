6f7667d2dc9a41a1081dfc07ee91be5415381f03
Chirag <chiragrrai44@gmail.com>
2026-01-28 10:46:36 +0530

    object usage

--- notepad/employee.java
+++ notepad/employee.java
@@ -6,13 +6,19 @@ class employee
     public static void main(String[] args)
     {
         employee chirag = new employee();
-        {
+        employee ashith = new employee();
             chirag.name="Chirag Rai";
             chirag.id=100;
             chirag.sal=1000000;
             System.out.println("Name"+chirag.name);
             System.out.println("ID"+chirag.id);
             System.out.println("Salary"+chirag.sal);
-        }
+            ashith.name="Ashith Rai";
+            ashith.id=101;
+            ashith.sal=1000001;
+            System.out.println("--------------------");
+            System.out.println("Name"+ashith.name);
+            System.out.println("ID"+ashith.id);
+            System.out.println("Salary"+ashith.sal);
     }
 }
