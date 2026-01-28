2f37da41a584a7436408e48410b365ec576f1c6b
Chirag <chiragrrai44@gmail.com>
2026-01-28 15:12:47 +0530

    parametrized constructor

--- notepad/parametrized.java
+++ notepad/parametrized.java
@@ -13,7 +13,14 @@ class parametrized
     public static void main(String[] args)
     {
         parametrized chirag = new parametrized(101,"chirag",21,"Male");
+        parametrized akshar = new parametrized(102,"akshar",24,"Male");
         System.out.println("Name "+chirag.id);
+        System.out.println("Age: "+chirag.age);
+        System.out.println("Gender: "+chirag.gender);
+        System.out.println("-------------------------");
+        System.out.println("Name "+akshar.id);
+        System.out.println("Age: "+akshar.age);
+        System.out.println("Gender: "+akshar.gender);
     }
 
     
