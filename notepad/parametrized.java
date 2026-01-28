a10d735a873e8361eabfdf4ccbf963445a2fac1b
Chirag <chiragrrai44@gmail.com>
2026-01-28 15:22:45 +0530

    parametrized constructor with user input

--- notepad/parametrized.java
+++ notepad/parametrized.java
@@ -1,28 +1,28 @@
+import java.util.Scanner;
 class parametrized
 {
     int id;
     String name;
     int age;
     String gender;
-         parametrized(int id1,String name1,int age1,String gender1){
+    Scanner sc = new Scanner(System.in);
+         parametrized(int id1,String name1,String gender1){
             System.out.println("Enter the Age");
             age = sc.nextInt();
+            id=id1;
             name=name1; // if name=name then use keyword "this".
-            age=age1;
             gender=gender1;
          }
     public static void main(String[] args)
     {
-        parametrized chirag = new parametrized(101,"chirag",21,"Male");
-        parametrized akshar = new parametrized(102,"akshar",24,"Male");
-        System.out.println("Name "+chirag.id);
+        parametrized chirag = new parametrized(101,"chirag","Male");
+        parametrized akshar = new parametrized(102,"akshar","Male");
+        System.out.println("Name "+chirag.name);
         System.out.println("Age: "+chirag.age);
         System.out.println("Gender: "+chirag.gender);
         System.out.println("-------------------------");
-        System.out.println("Name "+akshar.id);
+        System.out.println("Name "+akshar.name);
         System.out.println("Age: "+akshar.age);
         System.out.println("Gender: "+akshar.gender);
     }
-
-    
 }
