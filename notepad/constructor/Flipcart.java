2a56651a14509741bdc8e66097f3f768ca3838b0
Chirag <chiragrrai44@gmail.com>
2026-01-29 10:26:55 +0530

    create a flipcart database using constructor overloading

--- notepad/constructor/Flipcart.java
+++ notepad/constructor/Flipcart.java
@@ -14,7 +14,7 @@ class Flipcart
         this.gender=gender;
 
     }
-     public Flipcart(int id,String name,String address,int age)
+     public Flipcart(int id,String name,String address)
     {
          this.id=id;
         this.name=name;
@@ -22,18 +22,27 @@ class Flipcart
     }    
     public void printdata()
     {
-        System.out.println("StudentID: "+id);
-        System.out.println("Student name: "+name);
-        if(age!=0)
+     if(age==0)
         {
-            System.out.println("Professor age: "+age);
+        System.out.println("Seller ID: "+id);
+        System.out.println("Seller name: "+name);
+        System.out.println("Seller address: "+address);
         }
+     else
+        {   
+        System.out.println("User ID: "+id);
+        System.out.println("user name: "+name);
+        System.out.println("user address: "+address);
+        System.out.println("user Age: "+age);
+        System.out.println("user Gender: "+gender);
+        }
+       
     }
     public static void main(String[] args)
     {
-        conover chirag = new conover(101,"Chirag Rai");
-        conover anvith = new conover(102,"Anvith Chakote");
-        conover abhishek = new conover(202,"Abhishek",35);
+        Flipcart chirag = new Flipcart(101,"Chirag Rai","puttur",21,"Male");
+        Flipcart anvith = new Flipcart(102,"Anvith Chakote","Sullya",21,"Male");
+        Flipcart abhishek = new Flipcart(202,"Abhishek","Delhi");
         chirag.printdata();
         System.out.println("--------------------");
         anvith.printdata();
