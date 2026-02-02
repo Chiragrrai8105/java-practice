37732c84cfafc6ae826b65cf6f98db9f8b96ac55
Chirag <chiragrrai44@gmail.com>
2026-02-02 10:44:26 +0530

    deleted class

---
+++ notepad/utility class/circle.java
@@ -0,0 +1,16 @@
+public class circle
+{
+    private int radius;
+    public void setradius(int radius)
+    {
+        this.radius=radius;
+    }
+    public int getradius()
+    {
+        return this.radius=radius;
+    }
+    public circle(int radius)
+    {
+        setradius(radius);
+    }
+}
