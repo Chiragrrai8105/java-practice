51fb6ad6fab5cd79ae49bf3020a255cbf11428b1
Chirag <chiragrrai44@gmail.com>
2026-02-02 10:02:39 +0530

    Utility class

---
+++ notepad/encapsulation/two class/drivera.java
@@ -0,0 +1,8 @@
+public class drivera
+{
+    public static void main(String[] args)
+    {
+        System.out.println(utility.dia(c));
+        System.out.println(utility.area(c));
+    }
+}
---
+++ notepad/utility class/utility.java
@@ -0,0 +1,11 @@
+public class utility
+{
+    public static int dia(circle r)
+    {
+        return r.getradius()*2;
+    }
+    public static int area(circle r)
+    {
+        return 3.14*c.getradius()*c.getradius();
+    }
+}
