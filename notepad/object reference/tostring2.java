6c9b6a7611b5a8bbd49495ddc051fea1c8876882
Chirag <chiragrrai44@gmail.com>
2026-02-04 10:41:52 +0530

    object reference of toString() and print a statment using method call and encapsulation

---
+++ notepad/object reference/tostring2.java
@@ -0,0 +1,35 @@
+class chirag1
+{
+    public String name;
+    private int age;
+    public void setdemo(int age)
+    {
+        this.age=age;
+    }
+    public int getdemo()
+    {
+        return this.age;
+    }
+    public chirag1(String name,int age)
+    {
+        super();
+        this.name=name;
+
+    }
+    /*public String tostring1()
+    {
+        return "name="+"pooja"+"AGE="+21;
+    }*/
+}   
+public class tostring2
+{
+    public static void main(String[] args)
+    {
+        chirag1 c = new chirag1("pooja",21);
+        System.out.println("Name: "+c.name);
+        c.setdemo(25);
+        System.out.println(c.getdemo());
+
+
+    }
+}    
