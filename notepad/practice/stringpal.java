cb6b869d14ddae49db4711285a255f39a0b9acf5
Chirag <chiragrrai44@gmail.com>
2026-02-06 10:45:37 +0530

    palindrome string

--- notepad/practice/stringpal.java
+++ notepad/practice/stringpal.java
@@ -2,7 +2,7 @@ class stringpal
 {
     public static void main(String[] args)
     {
-        String s="chirag";
+        String s="malayalam";
         boolean bool=true;
         char br[] = new char[s.length()];
         char ar[] = new char[s.length()];
@@ -10,14 +10,16 @@ class stringpal
         for(int i=s.length()-1;i>=0;i--)
         {
             br[j]=s.charAt(i);
-            System.out.print(br[i]);
+            System.out.print(br[j]);
             j++;
         }
+        System.out.println("");
          for(int k=0;k<s.length();k++)
         {
             System.out.print(s.charAt(k));
             ar[k]=s.charAt(k);
         }
+        System.out.println("");
         for(int i=0;i<s.length();i++)
         {
             if(br[i]!=ar[i])
