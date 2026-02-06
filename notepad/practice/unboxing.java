526d5c625f3ac593a0c20d5c446ac6e471758912
Chirag <chiragrrai44@gmail.com>
2026-02-06 09:50:31 +0530

    wrapper class for boxing and unboxing

---
+++ notepad/practice/unboxing.java
@@ -0,0 +1,10 @@
+class unboxing
+{
+    public static void main(String[] args)
+    {
+        Integer i=10;
+        int a=i;//autoboxing
+        System.out.println(a);
+        //System.out.println(a.toString()); since 'a' is primitive data type it does not act as a reference variable.
+    }
+}
