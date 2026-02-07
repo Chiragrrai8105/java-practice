89480384ee1dc78f7af7ce9053f89c35ccd5af37
Chirag <chiragrrai44@gmail.com>
2026-02-07 19:15:11 +0530

    code to sqaure a number and comapare last digits of origin number and squared number

---
+++ notepad/extras/sqrcom.java
@@ -0,0 +1,25 @@
+class sqrcom
+{
+    public static void demo(int n)
+    {
+        int n1=n,n2=n,n3=n,count=0,mul=0;
+        n*=n;
+        while(n2>0)
+        {
+            n2/=10;
+            count++;
+        }
+        for(int i=0;i<count;i++)
+        {
+            mul*=10;
+        }
+        int rem=n%mul;
+        System.out.println(rem);
+        if(n3==rem)
+        {
+            System.out.println("Equal");
+        }
+        else
+            System.out.println("Not Equal");
+    }
+}
