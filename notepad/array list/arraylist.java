765d3e9f542f5dd404e7047fd647897f51fd6ba1
Chirag <chiragrrai44@gmail.com>
2026-02-07 19:06:48 +0530

    Array list

--- notepad/array list/arraylist.java
+++ notepad/array list/arraylist.java
@@ -18,5 +18,22 @@ class arraylist
         list.addFirst("Druvi");
         list.addLast("Bhuvana");
         System.out.println(list);
+        System.out.println("------------------------------");
+        ArrayList<String> wishlist = new ArrayList<String>();
+        wishlist.add("Bunglow");
+        wishlist.add("G-wagon G63");
+        wishlist.add("Defender octa");
+        wishlist.add("Rangrover AutoBiography");
+        wishlist.add("Lamborgini Urus");
+        wishlist.add("Landcruiser 500");
+        wishlist.add("rolls royce phantom");
+        wishlist.add("rolls royce cullinon");
+        wishlist.add("rolls royce spectre");
+        wishlist.add("rolls royce ghost");
+        wishlist.add("rolls royce phantom");
+        wishlist.add(1,"Kawasaki H2r");
+        wishlist.addFirst("Dugati");
+        wishlist.addLast("BMW s1000 rr");
+        System.out.println(wishlist);
     }
 }
