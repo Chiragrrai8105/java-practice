75bb9379880999f35a1d6b15fae5636bdeb95eae
Chirag <chiragrrai44@gmail.com>
2026-02-09 21:07:06 +0530

    code to check duplicates in an array

--- notepad/extras/duplicate.java
+++ notepad/extras/duplicate.java
@@ -16,4 +16,9 @@ class duplicate
     {
         return new duplicate();
     }
+    public static void main(String[] args)
+    {
+        int arr[]={1,4,3,5,3};
+        obj().demo(arr);
+    }
 }
