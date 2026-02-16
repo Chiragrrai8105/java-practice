b126c4665cecebf994740ea9a6c30440c95f665e
Chirag <chiragrrai44@gmail.com>
2026-02-16 16:34:29 +0530

    Factorial of a number using recursion

--- notepad/extras/factorial.java
+++ notepad/extras/factorial.java
@@ -1,13 +1,19 @@
-class factorial
+public class factorial
 {
-    public static void fact(int n)
+    public static int fact(int n)
     {
-        int a=n*n-1;
-        fact(n-1);
+        if(n==0)
+        {
+            return 1;
+        }
+        else 
+        {
+          return n*fact(n-1);
+        }
     }
     public static void main(String[] args)
     {
-        int n=3;
-        fact(n);
+        int n=4;
+        System.out.println(fact(n));
     }
 }
