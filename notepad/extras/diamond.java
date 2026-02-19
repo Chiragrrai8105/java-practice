7ff57b148f70712fcf6adfb2a05a4c55c65c46f0
Chirag <chiragrrai44@gmail.com>
2026-02-19 19:30:07 +0530

    to print numbers in diamond shape

---
+++ notepad/extras/diamond.java
@@ -0,0 +1,22 @@
+public class diamond
+{
+    public static void main(String[] args)
+    {
+        int num=1,n=6;
+        for(int i=1;i<=n;i++)
+        {
+            for(s=1;s<n;s++)
+            {
+                System.out.println(" ");
+            }
+            System.out.println(num++);
+            if(i>1)
+            {
+                for(int s=1;i<=(2*i-3);s++)
+                {
+                    System.out.println(" ");
+                }
+            }
+        }
+    }
+}
