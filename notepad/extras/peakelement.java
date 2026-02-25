6d6de51a7ac4661a7c266db0729f82a0c113474d
Chirag <chiragrrai44@gmail.com>
2026-02-25 13:36:40 +0530

    deleted classes

--- notepad/extras/peakelement.java
+++ notepad/extras/peakelement.java
@@ -15,7 +15,7 @@ class peakelement {
        return a;
     }
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,8,3};
+        int arr[]={1,2,3,4, 5, 9};
         System.out.println(demo(arr));
     }
 }
