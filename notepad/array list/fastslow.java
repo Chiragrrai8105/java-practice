5c487ecee0c41750891874b72fba1c93028a548b
Chirag <chiragrrai44@gmail.com>
2026-03-27 15:14:07 +0530

    fast and slow pointer

--- notepad/array list/fastslow.java
+++ notepad/array list/fastslow.java
@@ -2,7 +2,12 @@ class fastslow
 {
     public static void main(String[] args)
     {
+        int j=0,i;
         int arr[]={1,2,3,4,5};
-        
+        for(i=0;i<arr.length;i++)
+        {
+            j=j+2;
+        }
+        System.out.println(arr[i]);
     }
 }
